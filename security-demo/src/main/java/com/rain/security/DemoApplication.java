c7def30edaf2fe6c9a966217b51303f1fbce0701
Rain <renwei@souche.com>
2019-08-23 16:34:03 +0800

    hello world

---
+++ security-demo/src/main/java/com/rain/security/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.rain.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 左边
+ * @date 2019/8/23
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
